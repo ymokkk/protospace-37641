28c612f10ba657cde991397be62ca45fb3aa38a7
ymokkk <yamasaan126@gmail.com>
2022-03-11 17:02:17 +0900

    トップページ・詳細ページの遷移の実装

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,6 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!
-  before_action :move_to_index, except: [:edit]
+  before_action :authenticate_user!, except:[:index, :show]
+  #before_action :move_to_index, except: [:edit]
 
   def index
     @prototypes = Prototype.all
@@ -53,10 +53,10 @@ class PrototypesController < ApplicationController
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
 
-  def move_to_index
-    unless user_signed_in?
-      redirect_to action: :index
-    end
-  end
+  #def move_to_index
+    #unless user_signed_in?
+      #redirect_to action: :index
+    #end
+  #nd
 
 end
