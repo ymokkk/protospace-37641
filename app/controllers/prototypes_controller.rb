430d9fe36f073ef342965061b8d84036cd4cffd0
ymokkk <yamasaan126@gmail.com>
2022-03-11 16:08:30 +0900

    未ログインで遷移できないように実装

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,5 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, except: [:new, :edit, :destroy]
+  before_action :authenticate_user!
   before_action :move_to_index, except: [:edit]
 
   def index
