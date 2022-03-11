cc74120eab0b8feb7a615b782c922aca8d213bda
ymokkk <yamasaan126@gmail.com>
2022-03-11 14:37:26 +0900

    トップページにリダイレクトされる実装

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -27,6 +27,9 @@ class PrototypesController < ApplicationController
 
   def edit
     @prototype = Prototype.find(params[:id])
+    unless @prototype.user_id == current_user.id
+      redirect_to action: :index
+    end
   end
 
   def update
